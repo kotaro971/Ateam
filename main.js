a8820138b4dd68cd3cc3c23dd0261719c64c244a
kotaro971 <y22515@iwate-it.ac.jp>
2023-01-30 10:26:41 +0900

    こんちわ
    あは

---
+++ main.js
@@ -0,0 +1,30 @@
+
+let kaitoi
+let quiz = [
+    ["問題"]
+]
+let answer=[
+    ["答え"]
+]
+let answerjudge=[
+    ["比較"]
+]
+
+let count=[
+    ["正解数"]
+]
+
+function judge(){
+    if() {
+        //正解
+    }
+    else(){
+        //不正解
+    }
+}
+
+
+
+
+
+
