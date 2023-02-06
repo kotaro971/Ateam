b811b6e2e025446ad4f4a53767e7350811b31917
YuzuHaru <y22522@iwate-it.ac.jp>
2023-02-06 09:59:53 +0900

    途中経過
    関数の中身の追加

--- main.js
+++ main.js
@@ -1,40 +1,54 @@
 
 let kaitoi
 let quiz = [
-    ["問題"]
-]
-let answer=[
-    ["答え"]
-]
-let answerjudge=[
-    ["比較"]
-]
 
-let answercount=0
+    [""]
+]  //問題
+let answer = [
 
-let count=0
+    [""]
+]//答え(入力)
+let answerjudge = [
+
+]   //答え(比較用)
+
+let answercount = 0;
+
+let count = 0;
 
 //問題を表示させる関数
-function quest(){
-
+function quest() {
+    var mondai = document.getElementsByClassName("mondai");
+    mondai = quiz[count];
+    count += count;
 }
 //入力フォーム
-function write(){
-    var a=document.getElementsByName('a')
-answerjudge[count]=a
+function write() {
+    var ans = document.getElementsByName("ans");
+    answer[count] = ans;
+    button();
 
 }
 //解答ボタン
+function button() {
+    document.getElementsByClassName('ans-button').addEventListener('click', function () {
+        judge(answer);
+    })
+}
+
 //判定関数
-function judge(){
-    if(answer[count]==answerjudge[count]) {
+function judge() {
+    if (answer[count] == answerjudge[count]) {
         //正解
-        answercount+=answercount
-        count+=count
+        answercount += answercount;
+
+        window.alert("正解です!");
+        count += count;
     }
-    else{
+    else {
         //不正解
-        count+=count
+        window.alert("残念！正解は" + quiz[count] + "です。");
+        count += count;
     }
 }
 
