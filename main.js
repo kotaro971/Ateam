84d46db6525a8883edb626da32c4ee3045348b90
YuzuHaru <y22522@iwate-it.ac.jp>
2023-02-07 14:41:16 +0900

    iii
    iii

--- main.js
+++ main.js
@@ -1,55 +1,57 @@
 
 let kaitoi
-const quiz = ["img/380.png","img/384.png"];
+const quiz = [
+    "380.png", 
+    "384.png"];
 
 
 //問題
 let answer = [
 
-    ["クローバー"]//470
-    [""]
-    ["アーモンド"]//466
-    ["メロン"]//464
-    ["ほうこう"]//463
-    ["ダンサー"]//460
-    ["へんかん"]//459
-    ["やけど"]//458
-    ["よくじつ"]//454
-    ["すくーる"]//451
-    ["とかい"]//450
-    ["STUDENT"]//401
-    ["じゅうでん"]//400
-    ["ペリカン"]//397
-    ["ICE"]//395
-    ["明日"]//394
-    ["がっこう"]//389
-    ["あくま"]//386
-    ["ソーセージ"]//384
-    ["カメラ"]//380
-    ["ハロウィン"]//408
-    ["スライス"]//410
-    ["あたま"]//411
-    ["とけい"]//413
-    ["ロールケーキ"]//414
-    ["カメラ"]//418
-    ["こふんじだい"]//419
-    ["ねこ"]//420
-    ["ビル"]//424
-    ["ようき"]//425
-    ["ましかく"]//428
-    ["マウンド"]//431
-    ["きゅうこうばい"]//432
-    ["カタカナ"]//434
-    ["しきべつ"]//433
-    ["マスク"]//435
-    ["そのばしのぎ"]//436
-    ["タカラ"]//437
-    ["きなこ"]//439
-    ["しきしゃ"]//442
+
 
 ]//答え(入力)
 let answerjudge = [
-    
+    "クローバー",//470
+    "",
+    "アーモンド",//466
+    "メロン",//464
+    "ほうこう",//463
+    "ダンサー",//460
+    "へんかん",//459
+    "やけど",//458
+    "よくじつ",//454
+    "すくーる",//451
+    "とかい",//450
+    "STUDENT",//401
+    "じゅうでん",//400
+    "ペリカン",//397
+    "ICE",//395
+    "明日",//394
+    "がっこう",//389
+    "あくま",//386
+    "ソーセージ",//384
+    "カメラ",//380
+    "ハロウィン",//408
+    "スライス",//410
+    "あたま",//411
+    "とけい",//413
+    "ロールケーキ",//414
+    "カメラ",//418
+    "こふんじだい",//419
+    "ねこ",//420
+    "ビル",//424
+    "ようき",//425
+    "ましかく",//428
+    "マウンド",//431
+    "きゅうこうばい",//432
+    "カタカナ",//434
+    "しきべつ",//433
+    "マスク",//435
+    "そのばしのぎ",//436
+    "タカラ",//437
+    "きなこ",//439
+    "しきしゃ"//442
 ]   //答え(比較用)
 
 
@@ -61,33 +63,37 @@ let count = 0;
 
 quest();
 
-console.log(quiz[0]);
+
 //問題を表示させる関数
 function quest() {
-    document.querySelector(".quizimg").src="img/"  +quiz[count];
+    document.querySelector(".quizimg").src = "img/" + quiz[count];
     //"<img src=" +quiz[0]+">";
-    console.log(mondai)
+
 
 
     count += count;
 }
 //入力フォーム
-function write() {
-    var ans = document.getElementsByName("ans");
+var btn = document.getElementsByClassName('ans-button')[0].addEventListener('click', function () {
+   
+    var ans = document.getElementsByClassName("ans")[0];
     answer[count] = ans;
-    button();
+    
+console.log(answer[count].value);
+console.log(ans.value);
+judge();
 
-}
-//解答ボタン
-function button() {
-    document.getElementsByClassName('ans-button').addEventListener('click', function () {
-        judge(answer);
-    })
-}
+    //解答ボタン
+    //function button() {
+
+    //judge();
+
+});
+
 
 //判定関数
 function judge() {
-    if (answer[count] == answerjudge[count]) {
+    if (answer[count].value === answerjudge[count]) {
         //正解
         answercount += answercount;
 
@@ -96,7 +102,7 @@ function judge() {
     }
     else {
         //不正解
-        window.alert("残念！正解は" + quiz[count] + "です。");
+        window.alert("残念！正解は" + answerjudge[count] + "です。");
         count += count;
     }
 }
