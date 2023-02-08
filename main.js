793c7f3653e8c380ed09b95e6f9cab65d79de24f
YuzuHaru <y22522@iwate-it.ac.jp>
2023-02-08 15:44:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,47 @@
-
-let kaitoi
+//問題
 const quiz = [
     "380.png",
-    "384.png"];
+    "384.png",
+    "386.png",
+    "389.png",
+    "394.png",
+    "395.png",
+    "397.png",
+    "400.png",
+    "401.png",
+    "408.png",
+    "410.png",
+    "411.png",
+    "413.png",
+    "414.png",
+    "418.png",
+    "419.png",
+    "420.png",
+    "424.png",
+    "425.png",
+    "428.png",
+    "431.png",
+    "432.png",
+    "433.png",
+    "434.png",
+    "435.png",
+    "436.png",
+    "437.png",
+    "439.png",
+    "442.png",
+    "450.png",
+    "451.png",
+    "454.png",
+    "458.png",
+    "459.png",
+    "460.png",
+    "463.png",
+    "464.png",
+    "465.png",
+    "466.png",
+    "470.png"
+];
 
-
-//問題
-let answer = []//答え(入力)
 let answerjudge = [
     "クローバー",//470
     "あああ",
@@ -49,59 +84,54 @@ let answerjudge = [
     "きなこ",//439
     "しきしゃ"//442
 ]   //答え(比較用)
-
-
-
+var answer ;//答え(入力)
 
 let answercount = 0;
 
 let count = 0;
 
+var randamnum=0
+
 quest();
 
-    function quest() {
-        document.querySelector(".quizimg").src = "img/" + quiz[count];
-     
-    }
 //問題を表示させる関数
-//for (let i = 0; i <= 5; i++) {
+function quest() {
+    randamnum = Math.floor(Math.random() *40)
+    document.querySelector(".quizimg").src = "img/" + quiz[randamnum];
+}
 
-    //入力フォーム
-    var btn = document.getElementsByClassName('ans-button')[count].addEventListener('click', function () {
+//入力フォーム
+const btn = document.getElementsByClassName('ans-button')[count].addEventListener('click', function () {
 
-        const ans = document.getElementsByClassName("ans")[count];
-        answer[count] = ans;
-
-        console.log(answer[count].value);
-
-
-        judge();
-    });
-//}
-
-
+    var ans = document.getElementsByClassName("ans")[0];
+    answer = ans.value;
+    judge();
+});
 
 //判定関数
 function judge() {
-    if (answer[count].value == answerjudge[count]) {
-        //正解
-        answercount += answercount;
+    //正解
+    if (answer == answerjudge[randamnum]) {
+
+        answercount = answercount+1;
 
         console.log(count);
         window.alert("正解です!");
         count = count + 1;
         console.log(count);
         console.log(ans.value);
+        console.log(answer);
         document.getElementById("ans").value = '';
 
         return quest();
     }
     else {
         //不正解
-        window.alert("残念！正解は" + answerjudge[count] + "です。");
+        window.alert("残念！正解は" + answerjudge[randamnum] + "です。");
         count = count + 1;
         console.log(count);
         console.log(ans.value);
+        console.log(answer);
         document.getElementById("ans").value = '';
 
         return quest();
