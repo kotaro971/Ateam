05582280d5391ebed2dc841bc4dfce303bb56cce
RIRIKA <y22508@iwate-it.ac.jp>
2023-02-13 10:29:08 +0900

    nn

--- main.js
+++ main.js
@@ -138,7 +138,8 @@ function judge() {
     if (answer == answerjudge[randamnum]) {
         if (count < 11) {
             answercount = answercount + 1;
-            window.alert("正解です!");
+            swal('正解です!');
+            // window.alert("正解です!");
             document.getElementById("ans").value = '';
             console.log(count);
             return quest();
@@ -147,7 +148,8 @@ function judge() {
     else {
         //不正解
         if (count < 11) {
-            window.alert("残念！正解は" + answerjudge[randamnum] + "です。");
+            swal("残念！正解は" + answerjudge[randamnum] + "です。");
+            // window.alert("残念！正解は" + answerjudge[randamnum] + "です。");
             document.getElementById("ans").value = '';
             console.log(count);
             return quest();
@@ -164,8 +166,55 @@ function result() {
 
     const btnAdd = document.createElement("button");
     btnAdd.style.position = "absolute";
-    btnAdd.style.top = "38%";
-    btnAdd.style.left = "32%";
+    btnAdd.style.top = "400px";
+    btnAdd.style.left = "40%";
+    btnAdd.style.fontSize="50px";
+    btnAdd.style.color="#b1921b";
+        btnAdd.style.borderRight="4px solid #cea82c";
+        btnAdd.style.borderBottom= "10px solid #987c1e";
+        btnAdd.style.borderLeft= "4px solid #ffed8b";
+        btnAdd.style.borderRadius= 0;
+        btnAdd.style.background="-webkit-gradient(linear, right top, left top, from(#ffd75b), color-stop(30%, #fff5a0), color-stop(40%, #fffabe), color-stop(50%, #ffffdb), color-stop(70%, #fff5a0), to(#fdd456))";
+        btnAdd.style.background= "-webkit-linear-gradient(right, #ffd75b 0%, #fff5a0 30%, #fffabe 40%, #ffffdb 50%, #fff5a0 70%, #fdd456 100%)";
+        btnAdd.style.background= "linear-gradient(-90deg, #ffd75b 0%, #fff5a0 30%, #fffabe 40%, #ffffdb 50%, #fff5a0 70%, #fdd456 100%)";
+        btnAdd.style.textShadow="-1px -1px 1px #ffffd9";
+    // btnAdd.style.border="none";
+    // btnAdd.style.outline = "none";
+    // btnAdd.style.borderRadius="13px";
+    // btnAdd.style.boxShadow= "5px 6px 3px #cccbcb";
+    // btnAdd.style.color= "#000000";
+    // btnAdd.style.backgroundColor= "#c2c2c2";
+    // btnAdd.style.borderBottom="7px solid #494949";
+    btnAdd.addEventListener('mouseover', function() {
+        btnAdd.style.marginTop= "3px";
+        btnAdd.style.color= "#b1921b";
+        btnAdd.style.borderRight= "4px solid #cea82c";
+        btnAdd.style.borderBottom= "7px solid #987c1e";
+        btnAdd.style.borderLeft= "4px solid #ffed8b";
+        // btnAdd.style.marginTop= "3px";
+        // btnAdd.style.color= "#ff0000";
+        // btnAdd.style.background ="#c2c2c2";
+        // btnAdd.style.borderBottom= "2px solid #494949";
+        // btnAdd.style.marginBottom= "2px";
+    });
+    btnAdd.addEventListener('mouseleave', function() {
+        btnAdd.style.marginTop= "-3px";
+        btnAdd.style.color="#b1921b";
+        btnAdd.style.borderRight="4px solid #cea82c";
+        btnAdd.style.borderBottom= "10px solid #987c1e";
+        btnAdd.style.borderLeft= "4px solid #ffed8b";
+        btnAdd.style.borderRadius= 0;
+        btnAdd.style.background="-webkit-gradient(linear, right top, left top, from(#ffd75b), color-stop(30%, #fff5a0), color-stop(40%, #fffabe), color-stop(50%, #ffffdb), color-stop(70%, #fff5a0), to(#fdd456))";
+        btnAdd.style.background= "-webkit-linear-gradient(right, #ffd75b 0%, #fff5a0 30%, #fffabe 40%, #ffffdb 50%, #fff5a0 70%, #fdd456 100%)";
+        btnAdd.style.background= "linear-gradient(-90deg, #ffd75b 0%, #fff5a0 30%, #fffabe 40%, #ffffdb 50%, #fff5a0 70%, #fdd456 100%)";
+        btnAdd.style.textShadow="-1px -1px 1px #ffffd9";
+        // btnAdd.style.marginTop= "-3px";
+        // btnAdd.style.borderRadius="13px";
+        // btnAdd.style.boxShadow= "5px 6px 3px #cccbcb";
+        // btnAdd.style.color= "#000000";
+        // btnAdd.style.backgroundColor= "#c2c2c2";
+        // btnAdd.style.borderBottom="7px solid #494949";
+      });
 
     btnAdd.innerHTML = "結果発表";
     document.body.appendChild(btnAdd);
